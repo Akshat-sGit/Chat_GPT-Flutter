8f0e8c1cbc2f2dab84d58d50af3ed8ff7a76d632
Akshat-sGit <e21cseu0806@bennett.edu.in>
2023-07-25 23:20:36 +0530

    Input box created

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-const uniColor =  Color(0xFFFFFFFF);
+const uniColor = Color(0xFFFFFFFF);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -11,67 +11,70 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  final TextEditingController _controller = TextEditingController();
+
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+         Expanded(child: TextField(
+          controller: _controller,
+          decoration:const InputDecoration.collapsed(hintText: 'Send a message'),
+        )),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.send)),
+      ],
+    ).;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.blueGrey[900],
-      title:Text(
-        'Chat GPT',
-        style: GoogleFonts.poppins(
-          color: Colors.white,
-          fontSize: 30.0,
-          fontWeight: FontWeight.w900,
-        ),
-      ),
-    ),
-    // Write the code for an input box which will be placed at the end of the screen.
-    body: Column(
-      children: [
-        Expanded(
-          child: Container(
-            margin: const EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Colors.blueGrey[900],
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-          ),
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: Container(
-                margin: const EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey[900],
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey[900],
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-          ],
-        ),
-      ],
-    ),
-    drawer:Drawer(
-      backgroundColor: Colors.blueGrey[900],
-      child:const Center(
-        child: Text(
-          'Drawer',
-          style: TextStyle(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text(
+          'Chat GPT',
+          style: GoogleFonts.poppins(
             color: Colors.white,
             fontSize: 30.0,
-            fontWeight: FontWeight.bold,
+            fontWeight: FontWeight.w900,
           ),
         ),
       ),
-    ),
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.8,
+          )),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => const Text('Message'),
+            ),
+          ),
+          const Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          ),
+        ]
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.blueGrey[900],
+        child: const Center(
+          child: Text(
+            'Drawer',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

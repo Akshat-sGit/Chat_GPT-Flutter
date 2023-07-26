fc2ac6ce254bfb534d0c6a825f894df7fae25c91
Akshat-sGit <e21cseu0806@bennett.edu.in>
2023-07-27 00:28:43 +0530

    changes made

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,135 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:velocity_x/velocity_x.dart';
-import 'chatmessage.dart';
-import 'threedots.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:chat_gpt/Pallete.dart';
+// import 'package:velocity_x/velocity_x.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
-
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
-
 class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _controller = TextEditingController();
-  final List<ChatMessage> _messages = [];
-  late OpenAI? chatGPT;
-  bool _isImageSearch = false;
-
-  bool _isTyping = false;
-  @override
-  void initState() {
-    chatGPT = OpenAI.instance.build(
-        token: dotenv.env["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"],
-        baseOption: HttpSetup(receiveTimeout: 60000));
-    super.initState();
-  }
-  @override
-  void dispose() {
-    chatGPT?.close();
-    chatGPT?.genImgClose();
-    super.dispose();
-  }
-  // Link for api - https://beta.openai.com/account/api-keys
-  void _sendMessage() async {
-    if (_controller.text.isEmpty) return;
-    ChatMessage message = ChatMessage(
-      text: _controller.text,
-      sender: "user",
-      isImage: false,
-    );
-    setState(() {
-      _messages.insert(0, message);
-      _isTyping = true;
-    });
-    _controller.clear();
-    if (_isImageSearch) {
-      final request = GenerateImage(message.text, 1, size: "256x256");
-
-      final response = await chatGPT!.generateImage(request);
-      Vx.log(response!.data!.last!.url!);
-      insertNewData(response.data!.last!.url!, isImage: true);
-    } else {
-      final request =
-          CompleteText(prompt: message.text, model: kTranslateModelV3);
-
-      final response = await chatGPT!.onCompleteText(request: request);
-      Vx.log(response!.choices[0].text);
-      insertNewData(response.choices[0].text, isImage: false);
-    }
-  }
-  void insertNewData(String response, {bool isImage = false}) {
-    ChatMessage botMessage = ChatMessage(
-      text: response,
-      sender: "bot",
-      isImage: isImage,
-    );
-    setState(() {
-      _isTyping = false;
-      _messages.insert(0, botMessage);
-    });
-  }
-  Widget _buildTextComposer() {
-    return Row(
-      children: [
-        Expanded(
-          child: TextField(
-            controller: _controller,
-            onSubmitted: (value) => _sendMessage(),
-            decoration: const InputDecoration.collapsed(
-                hintText: "Question/description"),
-          ),
-        ),
-        ButtonBar(
-          children: [
-            IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () {
-                _isImageSearch = false;
-                _sendMessage();
-              },
-            ),
-            TextButton(
-                onPressed: () {
-                  _isImageSearch = true;
-                  _sendMessage();
-                },
-                child: const Text("Generate Image"))
-          ],
-        ),
-      ],
-    ).px16();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("ChatGPT")),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Flexible(
-                  child: ListView.builder(
-                reverse: true,
-                padding: Vx.m8,
-                itemCount: _messages.length,
-                itemBuilder: (context, index) {
-                  return _messages[index];
-                },
-              )),
-              if (_isTyping) const ThreeDots(),
-              const Divider(
-                height: 1.0,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: context.cardColor,
-                ),
-                child: _buildTextComposer(),
-              )
-            ],
+        appBar: AppBar(
+          title: const Text("ChatGPT"), 
+          leading: const Icon(Icons.menu),
+          centerTitle: true,
           ),
-        )
+          body: Column(
+            children: [
+              Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 120, 
+                      width: 120,
+                      margin: const EdgeInsets.only(top: 20), 
+                      decoration:const BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ]
+          ),
       );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'chat_screen.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+void main(){
   runApp(const MyApp());
 }
 

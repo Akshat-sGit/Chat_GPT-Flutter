410fc6822e0fa770d2542c3ec4c05b998199986c
Akshat-sGit <e21cseu0806@bennett.edu.in>
2023-07-27 00:11:27 +0530

    added pallete.dart

---
+++ lib/pallete.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color mainFontColor = Color.fromRGBO(19, 61, 95, 1);
+  static const Color firstSuggestionBoxColor = Color.fromRGBO(165, 231, 244, 1);
+  static const Color secondSuggestionBoxColor =
+      Color.fromRGBO(157, 202, 235, 1);
+  static const Color thirdSuggestionBoxColor = Color.fromRGBO(209, 243, 249, 1);
+  static const Color assistantCircleColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color borderColor = Colors.black;
+  static const Color whiteColor = Colors.white; 
+}
+
